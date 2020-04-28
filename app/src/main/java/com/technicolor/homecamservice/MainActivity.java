e476953896e1507b90cf043ad5b8ff0b3f628e8f
Hyeonjong Lee <hyeonjong.lee@technicolor.com>
2020-04-28 15:06:16 +0900

    foreground service

--- app/src/main/java/com/technicolor/homecamservice/MainActivity.java
+++ app/src/main/java/com/technicolor/homecamservice/MainActivity.java
@@ -1,17 +1,21 @@
 package com.technicolor.homecamservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.util.Log;
-
+import android.view.View;
 /***
  * Add base foreground service (sticky service)
  * start the service on boot completed
  ***/
+
 public class MainActivity extends AppCompatActivity {
 
     private PowerManager.WakeLock wakeLock;
@@ -30,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
             Log.i("IsAcquire", "Acquire");
             wakeLock.acquire();
         }
+    }
+
+    public void startService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
     }
 
     @Override
