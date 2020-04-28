496369dba51fe58afe42d5d886373062011a8065
Hyeonjong Lee <hyeonjong.lee@technicolor.com>
2020-04-28 09:54:28 +0900

    Patial wake lock

--- app/src/main/java/com/technicolor/homecamservice/MainActivity.java
+++ app/src/main/java/com/technicolor/homecamservice/MainActivity.java
@@ -2,13 +2,36 @@ package com.technicolor.homecamservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PowerManager.WakeLock wakeLock;
+
+
+    @SuppressLint("InvalidWakeLockTag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WakeLock Test");
+
+        if ((wakeLock != null) && (!wakeLock.isHeld())) {
+            Log.i("IsAcquire", "Acquire");
+            wakeLock.acquire();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        wakeLock.release();
+        super.onDestroy();
+
     }
 }
